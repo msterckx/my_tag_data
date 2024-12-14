c5f53d1217795fa4404d1f005c216a7c1146bed4
Your Name <you@example.com>
2024-12-14 08:45:52 +0000

    update all

---
+++ prepare_data.py
@@ -0,0 +1,6 @@
+start file: prepare_data.py
+task 1: data prep
+1) read csv with structured data into pandas dataframe and perform the following:
+- calculate correlations between features and make visual representation save it to 'images' folder
+
+end file: prepare_data.py
